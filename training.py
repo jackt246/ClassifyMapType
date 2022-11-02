e264cd8374eae78a7832e014513e8f9bcc469bc4
jturner <jturner@ebi.ac.uk>
2022-11-02 09:05:57 +0000

    dropout added

--- training.py
+++ training.py
@@ -93,16 +93,18 @@ data_augmentation = keras.Sequential(
 )
 
 model = Sequential([
-  layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+  data_augmentation,
+  layers.Rescaling(1./255),
   layers.Conv2D(16, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(32, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(64, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
+  layers.Dropout(0.4),
   layers.Flatten(),
   layers.Dense(128, activation='relu'),
-  layers.Dense(num_classes)
+  layers.Dense(num_classes, name="outputs")
 ])
 
 model.compile(optimizer='adam',
