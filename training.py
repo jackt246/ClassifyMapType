43cc92279fc7d305fca2cc7782fc430db61badf1
jturner <jturner@ebi.ac.uk>
2022-11-03 12:56:34 +0000

    updating dropout location

--- training.py
+++ training.py
@@ -97,11 +97,11 @@ model = Sequential([
   layers.Rescaling(1./255),
   layers.Conv2D(16, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
+  layers.Dropout(0.15),
   layers.Conv2D(32, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(64, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
-  layers.Dropout(0.15),
   layers.Flatten(),
   layers.Dense(128, activation='relu'),
   layers.Dense(num_classes, name="outputs")
