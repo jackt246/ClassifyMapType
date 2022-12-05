24c39f2124aa87141e7c3480589c7201088b626c
jturner <jturner@ebi.ac.uk>
2022-12-05 07:59:10 +0000

    re

--- training.py
+++ training.py
@@ -16,12 +16,12 @@ Tomograms = '{}/Tomograms'.format(Directory)
 batch_size = 32
 img_height = 100
 img_width = 100
-dropout = 0.05
+dropout = 0
 
 #Generate training dataset
 train_ds = tf.keras.utils.image_dataset_from_directory(
   Directory,
-  validation_split=0.85,
+  validation_split=0.80,
   subset="training",
   seed=123,
   image_size=(img_height, img_width),
@@ -30,7 +30,7 @@ train_ds = tf.keras.utils.image_dataset_from_directory(
 #Generate val dataset
 val_ds = tf.keras.utils.image_dataset_from_directory(
   Directory,
-  validation_split=0.15,
+  validation_split=0.20,
   subset="validation",
   seed=123,
   image_size=(img_height, img_width),
@@ -76,21 +76,19 @@ data_augmentation = keras.Sequential(
 )
 
 model = Sequential([
-  data_augmentation,
-  layers.Rescaling(1./255),
+  layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
   layers.Conv2D(16, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
-  #layers.Dropout(dropout),
   layers.Conv2D(32, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(64, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Flatten(),
   layers.Dense(128, activation='relu'),
-  layers.Dense(num_classes, name="outputs")
+  layers.Dense(num_classes)
 ])
 
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-6),
+model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
