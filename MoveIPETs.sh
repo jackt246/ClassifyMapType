e66b78ddf2306a9921683d180477213cc3e1af8f
jturner <jturner@ebi.ac.uk>
2023-01-12 16:06:20 +0000

    re

--- MoveIPETs.sh
+++ MoveIPETs.sh
@@ -8,4 +8,5 @@ INPUT=$(cat $FILE)
 for file in $INPUT
 do
 mv /hps/nobackup/gerard/emdb/TomogramCheck/Tomograms/$file/map/${file}.map.gz /hps/nobackup/gerard/emdb/TomogramCheck/NonTomograms/
+echo ${file}
 done
