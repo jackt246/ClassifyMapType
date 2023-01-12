beee6e4261bd08dd6ba103e558ff339b7ade08f5
jturner <jturner@ebi.ac.uk>
2023-01-12 16:20:48 +0000

    from main

--- training.py
+++ training.py
@@ -4,24 +4,25 @@ import matplotlib.pyplot as plt
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
+import pandas as pd
 
 #Data directories
 
-Directory = 'Classes/'
-NonTomograms = '{}/NonTomograms'.format(Directory)
+Directory = 'Classes500/'
+SubTomo = '{}/NonTomograms'.format(Directory)
 Tomograms = '{}/Tomograms'.format(Directory)
 
 
 #Set up information on the data
 batch_size = 32
-img_height = 100
-img_width = 100
-dropout = 0.2
+img_height = 300
+img_width = 300
+dropout = 0
 
 #Generate training dataset
 train_ds = tf.keras.utils.image_dataset_from_directory(
   Directory,
-  validation_split=0.80,
+  validation_split=0.8,
   subset="training",
   seed=123,
   image_size=(img_height, img_width),
@@ -30,7 +31,7 @@ train_ds = tf.keras.utils.image_dataset_from_directory(
 #Generate val dataset
 val_ds = tf.keras.utils.image_dataset_from_directory(
   Directory,
-  validation_split=0.20,
+  validation_split=0.2,
   subset="validation",
   seed=123,
   image_size=(img_height, img_width),
@@ -56,14 +57,18 @@ AUTOTUNE = tf.data.AUTOTUNE
 train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
+#normalise the data pixel values
 normalization_layer = layers.Rescaling(1./255)
 
 normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
 image_batch, labels_batch = next(iter(normalized_ds))
+first_image = image_batch[0]
+# Notice the pixel values are now in `[0,1]`.
+print(np.min(first_image), np.max(first_image))
 
 num_classes = len(class_names)
 
-#Add data augmentation to increase the amount of training data
+#Augment the data to increase the amount of training data, seems to help but not essential
 data_augmentation = keras.Sequential(
   [
     layers.RandomFlip("horizontal",
@@ -75,32 +80,40 @@ data_augmentation = keras.Sequential(
   ]
 )
 
+#Define the model
 model = Sequential([
-  layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+  data_augmentation,
+  layers.Rescaling(1./255),
   layers.Conv2D(16, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
+  layers.Dropout(dropout),
   layers.Conv2D(32, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(64, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
-  layers.Dropout(dropout),
+  layers.Conv2D(128, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
   layers.Flatten(),
-  layers.Dense(128, activation='relu'),
-  layers.Dense(num_classes)
+  layers.Dense(256, activation='relu'),
+  layers.Dense(num_classes, name="outputs")
 ])
 
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4),
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-6),
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
 model.summary()
+#chose number of epochs
+epochs=250
 
-epochs=100
+#train and save as a history object for plotting.
 history = model.fit(
   train_ds,
   validation_data=val_ds,
   epochs=epochs
 )
+
+#Plot a bunch of stats
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
 
@@ -121,12 +134,18 @@ plt.plot(epochs_range, loss, label='Training Loss')
 plt.plot(epochs_range, val_loss, label='Validation Loss')
 plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
-plt.savefig('Training_summary_50epoch_dropout02_TomogramCheck.png')
+figtitle='Training_summary_ImgSize500_learningrate1e6_epoch250.png'
+plt.savefig('Outputs/{}'.format(figtitle))
+print(figtitle)
 
-# Convert the model.
+# Convert the model to a tf lite model
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
 
+#Output the graph values
+df = pd.DataFrame(list(zip(acc, val_acc, loss, val_loss)), columns=['Accuracy', 'Val_Accuracy', 'Loss', 'Val_Loss'])
+df.to_csv('Outputs/{}.csv'.format(figtitle.strip('.png')))
+
 # Save the model.
-with open('model_TomogramCheck.tflite', 'wb') as f:
+with open('model_withIPET.tflite', 'wb') as f:
   f.write(tflite_model)
