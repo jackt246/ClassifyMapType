36a4bbcda623022e8e9b8677d3eadc406e84909e
jturner <jturner@ebi.ac.uk>
2023-01-19 09:10:45 +0000

    re

--- RunNetwork.py
+++ RunNetwork.py
@@ -1,75 +1,89 @@
 import tensorflow as tf
 import numpy as np
-
+import math
 import matplotlib.pyplot as plt
-
+import matplotlib.animation as animation
+import PIL
+import os
+import pathlib
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
 import mrcfile as mrc
 import sys
 
-def OpenMrc(filename):
-    # filename must be string
-    map = mrc.read(filename)
-    return map
 
-class CentralSlicer():
-    #Need standardised array shapes for machine learning
-    def __init__(self, array):
-        self.array = array
+class emMap():
+    # Need standardised array shapes for machine learning
+    def __init__(self, filename):
+        self.array = mrc.read(filename)
 
     def CentSliceZ(self):
-        #takes numpy array
+        # takes numpy array
         array = self.array
-        y,x,z = self.array.shape
-        midz = z//2
+        y, x, z = self.array.shape
+        midz = z // 2
         return self.array[:, :, midz]
 
     def CentSliceY(self):
-        #takes numpy array
-        y,x,z = self.array.shape
-        midx = x//2
-        midz = z//2
+        # takes numpy array
+        y, x, z = self.array.shape
+        midx = x // 2
+        midz = z // 2
         return self.array[:, midx, midz]
 
     def CentSliceX(self):
-        #takes numpy array
-        y,x,z = self.array.shape
-        midy = y//2
-        midz = z//2
+        # takes numpy array
+        y, x, z = self.array.shape
+        midy = y // 2
+        midz = z // 2
         return self.array[midy, :, midz]
+
+
+class runModel():
+    def __init__(self, modelPath):
+        self.modelPath = modelPath
+
+    def imageSetup(self, imgLocation):
+        img_height = 100
+        img_width = 100
+
+        img = tf.keras.utils.load_img(
+            imgLocation, target_size=(img_height, img_width)
+        )
+        imgArray = tf.keras.utils.img_to_array(img)
+        self.imgArray = tf.expand_dims(imgArray, 0)  # Create a batch
+
+    def runPrediction(self):
+        interpreter = tf.lite.Interpreter(model_path=self.modelPath)
+        classify_lite = interpreter.get_signature_runner('serving_default')
+        predictions_lite = classify_lite(rescaling_1_input=self.imgArray)['dense_1']
+        score_lite = tf.nn.softmax(predictions_lite)
+
+        class_names = ['Subtomogram Averaging', 'Tomogram']
+        print(
+            "This map is likely a {} with a {:.2f} percent confidence."
+            .format(class_names[np.argmax(score_lite)], 100 * np.max(score_lite))
+        )
+
+def Projector(array, axis):
+    arraySum = np.sum(array, axis=axis)
+    return arraySum
 
 
 #Open and pre-process map
 MapLocation = sys.argv[1]
-Map = OpenMrc(MapLocation)
-MapEdit = CentralSlicer(Map)
+MapEdit = emMap(MapLocation)
 Z = MapEdit.CentSliceZ()
 
 plt.imsave('ImageToClassify.png', Z, cmap='Greys')
 ImageLoc = 'ImageToClassify.png'
 
 #Set up information on the data
-img_height = 100
-img_width = 100
 
-img = tf.keras.utils.load_img(
-    ImageLoc, target_size=(img_height, img_width)
-)
-img_array = tf.keras.utils.img_to_array(img)
-img_array = tf.expand_dims(img_array, 0) # Create a batch
-
-
-#Import network
-TF_MODEL_FILE_PATH = 'model.tflite' # The default path to the saved TensorFlow Lite model
-interpreter = tf.lite.Interpreter(model_path=TF_MODEL_FILE_PATH)
-classify_lite = interpreter.get_signature_runner('serving_default')
-predictions_lite = classify_lite(rescaling_1_input=img_array)['dense_1']
-score_lite = tf.nn.softmax(predictions_lite)
-
-class_names = ['Not Tomogram', 'Tomogram']
-print(
-    "This map is likely a {} with a {:.2f} percent confidence."
-    .format(class_names[np.argmax(score_lite)], 100 * np.max(score_lite))
-)
+mapTypePredictor = runModel('model_resize.tflite')
+mapTypePredictor.imageSetup(ImageLoc)
+mapTypePredictor.runPrediction()
 
 
 
