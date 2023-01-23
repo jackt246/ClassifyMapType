ecd64b394d01bac5220f0ceb7611b67765ac67b0
jturner <jturner@ebi.ac.uk>
2023-01-23 10:49:13 +0000

    re

--- GPUTest.py
+++ GPUTest.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
 import tensorflow as tf
+import os
+
+os.system('module load cudnn-8.0.4.30-11.1-gcc-9.3.0-bbr3kjv')
 
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
