2a0bc686009a43b78e884bfffb5f1ece76035801
jturner <jturner@ebi.ac.uk>
2023-02-03 16:28:03 +0000

    updated run network

--- RunNetwork_OnCodon.py
+++ RunNetwork_OnCodon.py
@@ -8,15 +8,10 @@ import matplotlib.pyplot as plt
 import mrcfile as mrc
 import sys
 
-def OpenMrc(filename):
-    # filename must be string
-    map = mrc.read(filename)
-    return map
-
-class CentralSlicer():
+class emMap():
     #Need standardised array shapes for machine learning
-    def __init__(self, array):
-        self.array = array
+    def __init__(self, filename):
+        self.array = mrc.read(filename)
 
     def CentSliceZ(self):
         #takes numpy array
@@ -39,7 +34,46 @@ class CentralSlicer():
         midz = z//2
         return self.array[midy, :, midz]
 
-Folder = '/hps/nobackup/gerard/emdb/ClassifyMapType/Tomograms'
+class runModel():
+    def __init__(self, modelPath):
+        self.modelPath = modelPath
+
+    def imageSetup(self, imgLocation):
+        img_height = 100
+        img_width = 100
+
+        img = tf.keras.utils.load_img(
+            imgLocation, target_size=(img_height, img_width)
+        )
+        imgArray = tf.keras.utils.img_to_array(img)
+        self.imgArray = tf.expand_dims(imgArray, 0)  # Create a batch
+
+    def runPrediction(self):
+        interpreter = tf.lite.Interpreter(model_path=self.modelPath)
+        classify_lite = interpreter.get_signature_runner('serving_default')
+        predictions_lite = classify_lite(rescaling_1_input=self.imgArray)['dense_1']
+        score_lite = tf.nn.softmax(predictions_lite)
+
+    #Import network
+    TF_MODEL_FILE_PATH = 'Training_summary_fullDataSet_ImgSize100_learningrate1e3_epoch75_wdienetwork2_NETWORK.tflite'
+    interpreter = tf.lite.Interpreter(model_path=TF_MODEL_FILE_PATH)
+    classify_lite = interpreter.get_signature_runner('serving_default')
+    predictions_lite = classify_lite(rescaling_1_input=img_array)['dense_1']
+    score_lite = tf.nn.softmax(predictions_lite)
+
+        class_names = ['Subtomogram Averaging', 'Tomogram']
+        print(
+            "This map is likely a {} with a {:.2f} percent confidence."
+            .format(class_names[np.argmax(score_lite)], 100 * np.max(score_lite))
+        )
+
+        data = pd.DataFrame({'Map': file, 'Expected Type': Folder, 'Predicted Type': class_names[np.argmax(score_lite)],
+                             'Prediction score %': 100 * np.max(score_lite)}, index=[0])
+
+        return data
+
+
+Folder = 'SubtomogramAverages'
 FilesList = os.listdir(Folder)
 
 Results = pd.DataFrame(columns=['Map', 'Expected Type', 'Predicted Type', 'Prediction score %'])
@@ -47,44 +81,22 @@ Results = pd.DataFrame(columns=['Map', 'Expected Type', 'Predicted Type', 'Predi
 #Open and pre-process map
 
 for file in FilesList:
+    # Open and pre-process map
     MapLocation = '{}/{}'.format(Folder, file)
-    print(MapLocation)
-    Map = OpenMrc(MapLocation)
-    MapEdit = CentralSlicer(Map)
+    MapEdit = emMap(MapLocation)
     Z = MapEdit.CentSliceZ()
+    # Projected = Projector(Map, 2)
 
+    plt.imsave('ImageToClassify_subtomo.png', Z, cmap='Greys')
+    ImageLoc = 'ImageToClassify_subtomo.png'
 
-    plt.imsave('ImageToClassify_tomo.png', Z, cmap='Greys')
-    ImageLoc = 'ImageToClassify_tomo.png'
+    # Set up information on the data
 
-    #Set up information on the data
-    img_height = 100
-    img_width = 100
-
-    img = tf.keras.utils.load_img(
-    ImageLoc, target_size=(img_height, img_width)
-    )
-    img_array = tf.keras.utils.img_to_array(img)
-    img_array = tf.expand_dims(img_array, 0) # Create a batch
-
-
-    #Import network
-    TF_MODEL_FILE_PATH = '50epoch_dropout02_model_15122022.tflite'
-    interpreter = tf.lite.Interpreter(model_path=TF_MODEL_FILE_PATH)
-    classify_lite = interpreter.get_signature_runner('serving_default')
-    predictions_lite = classify_lite(rescaling_1_input=img_array)['dense_1']
-    score_lite = tf.nn.softmax(predictions_lite)
-
-    class_names = ['Not Tomogram', 'Tomogram']
-    print(
-        "This map is likely a {} with a {:.2f} percent confidence."
-        .format(class_names[np.argmax(score_lite)], 100 * np.max(score_lite))
-    )
-
-    data = pd.DataFrame({'Map': file, 'Expected Type': Folder, 'Predicted Type': class_names[np.argmax(score_lite)], 'Prediction score %': 100 * np.max(score_lite)}, index=[0])
-    print(data)
+    mapTypePredictor = runModel('model_resize.tflite')
+    mapTypePredictor.imageSetup(ImageLoc)
+    data = mapTypePredictor.runPrediction()
     Results = Results.append(data, ignore_index=True)
 
 print(Results)
-Results.to_csv('results_tomo.csv'.format(Folder))
+Results.to_csv('results_{}_subtomo.csv'.format(Folder))
 
