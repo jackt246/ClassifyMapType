e77dad90a23cc96797f2b466cb402e8a70b21dbd
jturner <jturner@ebi.ac.uk>
2023-02-13 18:14:03 +0000

    re

--- GPUTest.py
+++ GPUTest.py
@@ -6,3 +6,8 @@ import os
 os.system('module load cudnn-8.0.4.30-11.1-gcc-9.3.0-bbr3kjv')
 
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+if tf.test.gpu_device_name():
+    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+else:
+   print("Please install GPU version of TF")
