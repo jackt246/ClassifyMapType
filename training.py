2cfe2f2532009aaa60ad38538d462bee8537901f
jturner <jturner@ebi.ac.uk>
2023-02-21 09:31:49 +0000

    re

--- training.py
+++ training.py
@@ -29,7 +29,7 @@ dropout = 0
 #Generate training dataset
 train_ds = tf.keras.utils.image_dataset_from_directory(
   Directory,
-  validation_split=0.8,
+  validation_split=0.2,
   subset="training",
   seed=123,
   image_size=(img_height, img_width),
@@ -94,12 +94,8 @@ model = Sequential([
   layers.MaxPooling2D(),
   layers.Conv2D(128, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
-  layers.Conv2D(256, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(512, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
   layers.Flatten(),
-  layers.Dense(1024, activation='relu'),
+  layers.Dense(256, activation='relu'),
   layers.Dense(num_classes, name="outputs")
 ])
 
@@ -109,7 +105,7 @@ model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4),
 
 model.summary()
 #chose number of epochs
-epochs=10
+epochs=5
 
 #train and save as a history object for plotting.
 history = model.fit(
@@ -139,7 +135,7 @@ plt.plot(epochs_range, loss, label='Training Loss')
 plt.plot(epochs_range, val_loss, label='Validation Loss')
 plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
-figtitle='Training_summary_ImgSize100_learningrate1e4_epoch10_SGD_noaug_dropout0_02dataset_02val_DEEP.png'
+figtitle='Training_summary_ImgSize100_learningrate1e4_epoch5_SGD_noaug_dropout0_fullds_02val.png'
 plt.savefig('Outputs/{}'.format(figtitle))
 print(figtitle)
 
@@ -152,5 +148,5 @@ df = pd.DataFrame(list(zip(acc, val_acc, loss, val_loss)), columns=['Accuracy', 
 df.to_csv('Outputs/{}.csv'.format(figtitle.strip('.png')))
 
 # Save the model.
-with open('Training_summary_ImgSize100_learningrate1e4_epoch10_SGD_noaug_dropout0_02dataset_02val_DEEP.tflite', 'wb') as f:
+with open('Training_summary_ImgSize100_learningrate1e4_epoch5_SGD_noaug_dropout0_fullds_02val', 'wb') as f:
   f.write(tflite_model)
