5e194d9919efa7c427b32fa7a9ce74a7a3245cfa
jturner <jturner@ebi.ac.uk>
2023-06-05 11:01:49 +0100

    add dropout function and clean up variables

--- trainingVolumes.py
+++ trainingVolumes.py
@@ -101,7 +101,7 @@ input_shape = (200, 200, 200, 1)
 model = Sequential([
     layers.Conv3D(32, (3, 3, 3), activation='relu', input_shape=input_shape),
     layers.MaxPooling3D((2, 2, 2)),
-    layers.dropout(dropout),
+    layers.Dropout(dropout),
     layers.Conv3D(64, (3, 3, 3), activation='relu'),
     layers.MaxPooling3D((2, 2, 2)),
     layers.Conv3D(128, (3, 3, 3), activation='relu'),
