98ee6ddf9ccea21ea52dd4c5dc4e10d355b46a30
jturnerebi <84771194+jturnerebi@users.noreply.github.com>
2024-03-23 20:02:58 +0000

    fixing

--- trainingVolumes.py
+++ trainingVolumes.py
@@ -169,8 +169,8 @@ y_pred_classes = np.argmax(y_pred_accumulated, axis=1)
 # Convert one-hot encoded labels to single-label integers
 y_true_single_label = np.argmax(y_true_accumulated)
 
-print(y_pred_accumulated)
-print(y_true_accumulated)
+print(y_pred_accumulated.shape())
+print(y_true_accumulated.shape())
 print(y_pred_classes)
 print(y_true_single_label)
 
