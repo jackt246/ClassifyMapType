f347e132a2f3ad7c50d4fb0d803c9b3b27fe111d
jturnerebi <84771194+jturnerebi@users.noreply.github.com>
2024-04-18 16:53:29 +0100

    prepping for copy files

--- CopyMaps.sh
+++ CopyMaps.sh
@@ -3,8 +3,8 @@
 #read -p "Enter path for text file which lists maps to be copied: " FILE
 #read -p "Enter path to folder you wish to copy these files to: " FolderName
 
-FILE=STA2024.txt
-FolderName=ValidationData_NotForTraining/STA/
+FILE=Helical2024.txt
+FolderName=ValidationData_NotForTraining/Helical/
 
 INPUT=$(cat $FILE)
 
