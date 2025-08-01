4c44ada818e39da7035b6555e0a8ea820855b94a
jack <jackt246@gmail.com>
2025-08-01 11:48:09 +0100

    upload volume training

--- training.py
+++ training.py
@@ -65,8 +65,6 @@ normalization_layer = layers.Rescaling(1./255)
 normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
 image_batch, labels_batch = next(iter(normalized_ds))
 first_image = image_batch[0]
-# Notice the pixel values are now in `[0,1]`.
-print(np.min(first_image), np.max(first_image))
 
 num_classes = len(class_names)
 
